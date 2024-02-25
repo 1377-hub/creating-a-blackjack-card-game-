77aca6b99488afa0fe8f240aaad2d624630d1535
Nehemia Kibungei <nehemiahkibungei@gmail.com>
2024-02-25 23:59:15 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+let firstCard =10
+
+let secondCard=15
+
+
+let sum = firstCard + secondCard 
+
+if (sum < 21){
+    console.log("You have a new card") 
+    }else if(sum === 21){
+        console.log("You've gotten the black Jack")    
+}else {
+    console.log("You are out of the game!")
+}
